fee3e00f592614dce8e779513d53e19a88920d08
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-10-18 10:01:59 +0530

    Setup static file access

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require("express-ejs-layouts");
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(cookieParser());
+
+app.use(express.static('./assests'));
+
+app.use(expressLayouts);
+
+// extract style and scripts from sub-pages into layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
